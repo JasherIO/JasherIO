9331adc46e745c32c5236429dd1575531c705e99
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-09 19:20:55 -0600

    glob

--- .github/actions/files/index.js
+++ .github/actions/files/index.js
@@ -1,7 +1,9 @@
 import core from '@actions/core';
 import github from '@actions/github';
+import multimatch from 'multimatch';
 
 const GH_PERSONAL_ACCESS_TOKEN = core.getInput('GH_PERSONAL_ACCESS_TOKEN');
+const patterns = JSON.parse(core.getInput('patterns'));
 const octokit = github.getOctokit(GH_PERSONAL_ACCESS_TOKEN);
 
 const repo = github.context.payload.repository.name;
@@ -16,7 +18,8 @@ async function get_files() {
   const responses = await Promise.all(refs.map(get_commit));
   const files = responses.flatMap(response => response.data.files);
   const filenames = files.flatMap(file => file.filename);
-  return Array.from(new Set(filenames));
+  const unique_filenames = Array.from(new Set(filenames));
+  return multimatch(unique_filenames, patterns);
 }
 
 (async () => {
