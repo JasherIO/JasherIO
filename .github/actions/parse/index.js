52dc08b4e6a30184fd06167055d1b4861f734b34
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-10 09:24:19 -0600

    parse stub, read input from files job

---
+++ .github/actions/parse/index.js
@@ -0,0 +1,35 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+const GH_PERSONAL_ACCESS_TOKEN = core.getInput('GH_PERSONAL_ACCESS_TOKEN');
+const files = JSON.parse(core.getInput('files'));
+const octokit = github.getOctokit(GH_PERSONAL_ACCESS_TOKEN);
+
+const repo = github.context.payload.repository.name;
+const owner = github.context.payload.repository.owner.name;
+
+console.log(files);
+
+// function get_file(path) {
+//   return octokit.request('GET /repos/{owner}/{repo}/contents/{path}', { owner, repo, path });
+// };
+
+// function hasValidFrontmatter(attributes) {
+//   return attributes?.title && attributes?.description && attributes?.keywords && attributes?.date && attributes?.category;
+// };
+
+// function parse(content) {
+
+// };
+
+// async function get_parsed_files() {
+//   const responses = await Promise.all(files.map(get_file));
+
+// }
+
+// (async () => {
+//   const files = await get_files();
+//   core.setOutput("files", files.toString());
+
+//   console.log(files);
+// })();
