688e634086247da60624e0700239c395c3d0f31c
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-09 18:44:38 -0600

    test actually getting changed files

--- .github/actions/files/index.js
+++ .github/actions/files/index.js
@@ -2,25 +2,26 @@ import core from '@actions/core';
 import github from '@actions/github';
 
 const GH_PERSONAL_ACCESS_TOKEN = core.getInput('GH_PERSONAL_ACCESS_TOKEN');
-const commit_refs = core.getInput('commit_refs');
-// const octokit = github.getOctokit(GH_PERSONAL_ACCESS_TOKEN);
+const commits = core.getInput('commits');
+const refs = commits.map(commit => commit.id);
+const octokit = github.getOctokit(GH_PERSONAL_ACCESS_TOKEN);
 
-const repository = github.context.repository;
-console.log('repository: ', repository);
-console.log('commit_refs: ', commit_refs);
+const repository = github.context;
+console.log('context: ', context);
+console.log('commits: ', commits);
 
-// function get_commit({ repository, ref }) {
-//   return octokit.request('GET /repos/{repository}/commits/{ref}', { repository, ref });
-// };
+function get_commit({ repository, ref }) {
+  return octokit.request('GET /repos/{repository}/commits/{ref}', { repository: 'JasherIO/JasherIO', ref });
+};
 
-// async function get_files({ repository, refs }) {
-//   const responses = await Promise.all(refs.map(ref => get_commit({ repository, ref })));
-//   const files = responses.flatMap(response => response.data.files);
-//   const filenames = files.flatMap(file => file.filename);
-//   return Array.from(new Set(filenames));
-// }
+async function get_files({ repository, refs }) {
+  const responses = await Promise.all(refs.map(ref => get_commit({ repository, ref })));
+  const files = responses.flatMap(response => response.data.files);
+  const filenames = files.flatMap(file => file.filename);
+  return Array.from(new Set(filenames));
+}
 
-// (async () => {
-//   const files = await get_files({ repository, refs });
-//   console.log(files);
-// })();
+(async () => {
+  const files = await get_files({ repository, refs });
+  console.log(files);
+})();
