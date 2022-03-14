0a758b6f91660534800c7bef867df2e36d33ee65
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-14 12:10:37 -0500

    return markdown

--- .github/actions/upsert/index.js
+++ .github/actions/upsert/index.js
@@ -32,16 +32,17 @@ function parse(file) {
   const slug = file.data.path.replace('.md', '');
   
   const decoded = Buffer.from(file.data.content, 'base64').toString();
-  const { attributes: frontmatter, body } = parse_frontmatter(decoded);
+  const { attributes: frontmatter, body: markdown } = parse_frontmatter(decoded);
 
-  const html = marked(body);
-  const stats = reading_time(body);
+  const html = marked(markdown);
+  const stats = reading_time(markdown);
 
   return {
     slug,
     frontmatter,
     html,
-    stats
+    stats,
+    markdown
   };
 };
 
