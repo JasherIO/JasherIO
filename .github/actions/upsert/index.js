83e6817a7d5b40c07b7115e407b0857d1216c5a1
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-17 10:05:06 -0500

    event checking and execution

--- .github/actions/upsert/index.js
+++ .github/actions/upsert/index.js
@@ -1,10 +1,11 @@
 import core from '@actions/core';
-import github from '@actions/github';
+import github, { context } from '@actions/github';
 import glob from '@actions/glob';
 import multimatch from 'multimatch';
 import parse_frontmatter from 'front-matter';
 import { marked } from 'marked';
 import reading_time from 'reading-time';
+import { promises as fs } from 'fs/promises';
 
 function get_commit({ octokit, owner, repo, ref }) {
   return octokit.request('GET /repos/{owner}/{repo}/commits/{ref}', { owner, repo, ref });
@@ -44,38 +45,38 @@ async function get_contents(paths) {
 };
 
 async function main() {
-  // if not push or dispatch, return
+  if (!(content.eventName in ['push', 'workflow_dispatch']))
+    return;
 
   const GH_PERSONAL_ACCESS_TOKEN = core.getInput('GH_PERSONAL_ACCESS_TOKEN');
   const patterns = ['data/posts/*.md'];
 
   const octokit = github.getOctokit(GH_PERSONAL_ACCESS_TOKEN);
 
-  console.log(github.context);
-
   const repo = github.context.payload.repository.name;
   const owner = github.context.payload.repository.owner.name;
-  const refs = github.context.payload.commits.map(commit => commit.id);
-  
-  
 
-  // let files = [];
+  let files = [];
 
-  // // if push, get changed files
-  // const changed = await get_changed_filenames({ octokit, owner, repo, refs });
-  // const unique = Array.from(new Set(changed));
-  // files = multimatch(unique, patterns);
+  if (context.eventName === 'push') {
+    const refs = github.context.payload.commits.map(commit => commit.id);
+    const changed = await get_changed_filenames({ octokit, owner, repo, refs });
+    const unique = Array.from(new Set(changed));
+    files = multimatch(unique, patterns);
+  } else {
+    files = await get_all_filesnames();
+  }
 
-  // // if dispatch, get all files
-  // files = await get_all_filesnames();
+  console.log('files: ', files);
 
-  // // get file contents and parse
-  // const contents = await get_contents(files);
-  // const parsed = contents.map(parse);
+  // get file contents and parse
+  const contents = Promise.all(files.map(file => fs.readFile(file, { encoding: 'utf8' })))
+  console.log('contents: ', contents);
+  const parsed = contents.map(parse);
 
-  // const output = JSON.stringify(parsed);
-  // core.setOutput('files: ', output);
-  // console.log(output);
+  const output = JSON.stringify(parsed);
+  core.setOutput('files: ', output);
+  console.log(output);
 };
 
 main();
